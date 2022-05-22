a6860ccaa60244588350fe291956e88edaa9d6a9
jackzacarias <goldenhorsescountry@gmail.com>
2022-05-21 22:50:05 -0300

    Aula 05 concluida

--- src/main/java/aula05/Aula05.java
+++ src/main/java/aula05/Aula05.java
@@ -12,6 +12,26 @@ package aula05;
 public class Aula05 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        ContaBanco p1 = new ContaBanco();
+        p1.setNumConta(1111);
+        p1.setDono("Jubileu");
+        p1.abrirConta("CC");
+
+        ContaBanco p2 = new ContaBanco();
+        p2.setNumConta(2222);
+        p2.setDono("Cleuza");
+        p2.abrirConta("CP");
+
+        p1.depositar(100);
+        p2.depositar(500);
+        p2.sacar(100);
+
+        p1.sacar(150);
+        p1.fecharConta();
+
+        p2.estadoAtual();
+        p1.estadoAtual();
     }
+
 }
+
--- src/main/java/aula05/ContaBanco.java
+++ src/main/java/aula05/ContaBanco.java
@@ -1,34 +1,87 @@
 package aula05;
 
+import javax.swing.*;
+
 public class ContaBanco {
     //Atributos
     public Integer numConta;
     protected String tipo;
     private String dono;
-    private Float saldo;
+    private float saldo;
     private Boolean status;
 
      //Metodos personalizados
+    public void estadoAtual(){
+        System.out.println("-------------------------------------------------");
+        System.out.println("Conta: "+this.getNumConta());
+        System.out.println("Tipo: "+this.getTipo());
+        System.out.println("Dono(a)"+this.getDono());
+        System.out.println("Saldo: "+this.getSaldo());
+        System.out.println("Status: " +this.getStatus());
+    }
 
-    public void abrirConta() {
-
+    public void abrirConta(String t) {
+        this.setTipo(t);
+        this.setStatus(true);
+         if (t.equals("CC")) {
+            this.setSaldo(50);
+         } else if (t.equals("CP")){
+             this.setSaldo(150);
+         }
+        JOptionPane.showConfirmDialog(null, "Conta foi Aberta com sucesso!");
     }
     public  void fecharConta(){
-
+        if (this.getSaldo() > 0){
+            JOptionPane.showConfirmDialog(null,"Conta não pode ser fechada porque ainda tem dinheiro nela, Sr."+this.getDono());
+        }else if (this.getSaldo() < 0){
+            JOptionPane.showConfirmDialog(null, "Conta não pode ser fechada por que esta em débito, Sr. "+this.getDono());
+        }else{
+            setStatus(false);
+            JOptionPane.showConfirmDialog(null, "Conta foi fechada com sucesso, Sr. "+this.getDono());
+        }
     }
-    public void depositar(){
-
+    public void depositar(float v){
+        if (this.getStatus() .equals(true)){
+            this.setSaldo(getSaldo() + v);
+            JOptionPane.showConfirmDialog(null, "Deposito realizado na conta " +this.getDono()+ " com sucesso!");
+        }else{
+            JOptionPane.showConfirmDialog(null, "Imporssivel depositar nessa conta por que esta fechada.");
+        }
     }
-    public  void sacar(){
-
+    public  void sacar(float v){
+        if (this.getStatus().equals(true)){
+            if (this.getSaldo() >= v) {
+                this.setSaldo(this.getSaldo() - v);
+                JOptionPane.showConfirmDialog(null, "Saque realizado na conta de "+this.getDono()+" do valor "+v);
+            } else  {
+                JOptionPane.showConfirmDialog(null, "Saldo insuficiente. \n Seu saldo atual é de: " + this.getSaldo());
+            }
+        }else {
+            JOptionPane.showConfirmDialog(null, "Imporssivel sacar de uma conta fechada!");
+        }
     }
     public void pagarMensal(){
-
+        int v = 0;
+        if (this.getTipo().equals("CC")) {
+            v = 12;
+        } else if (this.getTipo().equals("CP")) {
+            v = 20;
+        }
+        if (this.getStatus().equals(true)) {
+            if (this.getSaldo() > v){
+                this.setSaldo(this.getSaldo() - v);
+            } else{
+                JOptionPane.showConfirmDialog(null, "mensalidade paga com sucesso por "+this.getDono());
+            }
+        }else{
+            JOptionPane.showConfirmDialog(null, "Imporssivel pagar um conta fechada!");
+        }
     }
 
     // metado especial contrutor
     public void ContaBanco(){
-
+        this.status = false;
+        this.saldo = 0;
     }
 
     public void setNumConta(Integer numConta) {
@@ -55,11 +108,11 @@ public class ContaBanco {
         return dono;
     }
 
-    public void setSaldo(Float saldo) {
+    public void setSaldo(float saldo) {
         this.saldo = saldo;
     }
 
-    public Float getSaldo() {
+    public float getSaldo() {
         return saldo;
     }
 
